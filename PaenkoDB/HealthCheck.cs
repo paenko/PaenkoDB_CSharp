ba039b33d15bdef30b0bb70ec3fe9d0e0dfcb3a3
Florian Hanko <FlorianHanko@hotmail.com>
2017-01-07 20:43:06 +0100

    Added timed healthchecks

--- PaenkoDB/HealthCheck.cs
+++ PaenkoDB/HealthCheck.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace PaenkoDB
 {
     public class HealthCheck
     {
+        static Timer _Timer;
+        static Action<List<PaenkoNode>> TimerElapsedCallback;
+        static List<PaenkoNode> TimerElapsedToCheck;
+
         public async static Task<List<PaenkoNode>> CheckHealth(List<PaenkoNode> toCheck)
         {
             List<PaenkoNode> Alive = new List<PaenkoNode>();
@@ -16,6 +21,18 @@ namespace PaenkoDB
                 if(await NetworkHandler.CheckAliveAsync(pn.NodeLocation.ip)) Alive.Add(pn);
             }
             return Alive;
+        }
+
+        public static void SetTimedCheck(List<PaenkoNode> toCheck, Action<List<PaenkoNode>> callback, int intervalInSeconds)
+        {
+            TimerElapsedCallback = callback;
+            TimerElapsedToCheck = toCheck;
+            _Timer = new Timer(Elapsed, null, TimeSpan.Zero, TimeSpan.FromSeconds(intervalInSeconds));
+        }
+
+        static async void Elapsed(object state)
+        {
+            TimerElapsedCallback(await CheckHealth(TimerElapsedToCheck));
         }
     }
 }
